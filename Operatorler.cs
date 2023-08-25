a2dbc06d8be3daa0e63dc67cada1a9d268b22aaf
Choq0 <141420156+Choq0@users.noreply.github.com>
2023-08-25 12:36:56 +0300

    Create Operatorler.cs

---
+++ Operatorler.cs
@@ -0,0 +1,34 @@
+using System;
+
+// Atama ve işlemli Atama
+
+int x = 5;
+int y = 9;
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+y += 2;
+x += 1;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+x *= 2;
+Console.WriteLine(x);
+
+// İlişkisel Operatorler 
+
+int a = 5 ;
+int b = 6 ;
+
+bool sonuc = a<b ;
+
+Console.WriteLine(sonuc);
+sonuc = a > b ;
+Console.WriteLine(sonuc);
+
+
+// % mod alır.
+
+int sonuc1 = 20%3 ;
+Console.WriteLine(sonuc1);
